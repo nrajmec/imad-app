abe1c33f408d9433610e6d33d96cccc5f933feea
nrajmec <31283992+nrajmec@users.noreply.github.com>
2017-09-08 21:11:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article one is requested');
+  res.send('Article one is requested');
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile('Article two is requested');
+  res.send('Article two is requested');
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile('Article three is requested');
+  res.send('Article three is requested');
 });
 
 app.get('/ui/style.css', function (req, res) {

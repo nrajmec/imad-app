d37318dd7025b5cfcd0fba8fd8bb2c3135bba234
nrajmec <31283992+nrajmec@users.noreply.github.com>
2017-09-11 21:38:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ var articles= {
     }
 };
 
-app.get('/;articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });

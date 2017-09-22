8725c5c6885dd14c109c377d978a57147d14a2e1
nrajmec <31283992+nrajmec@users.noreply.github.com>
2017-09-22 23:26:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,15 +28,28 @@ var submit = document.getElementById('submit_btn');
 
 submit.onclick = function () {
     
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    var request = new XMLHttpRequest();
     
-    var list='';
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var names = request.responseText;
+                name= JSON.parse(names);
+                var list='';
+                
+                for(var i = 0; i < names.length;i++)
+                {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
     
-    for(var i = 0; i < names.length;i++)
-    {
-        list += '<li>' + names[i] + '</li>';
-    }
+    request.open('GET', 'http://naveenmechsai.imad.hasura-app.io/subbit-name?name=' + name, true);
+    request.send(null);
     
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    
 };
